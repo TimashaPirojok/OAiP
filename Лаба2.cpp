74350e817ae795fe213f97c3f7cc3adf1b7d31f4
TimashaPirojok <72824293+TimashaPirojok@users.noreply.github.com>
2020-10-13 23:15:20 +0300

    Add files via upload

---
+++ Лаба2.cpp
@@ -0,0 +1,45 @@
+﻿//laba 2 
+#include <iostream>
+
+int main()
+{
+	int a[100][100];
+	int b[100][100];
+	int c[100][100] = { 0 };
+	int i, j, m, n;
+	setlocale(LC_ALL, "Russia");
+	printf("Введите количество строк, n=");
+	scanf_s("%d", &n);
+	 printf("Введите количество столбцов, m=");
+	  scanf_s("%d", &m);
+	printf ("Введите элемент матрицы a\n");
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j < m; j++)
+		{
+			printf ("a[%d][%d]=",i,j );
+			scanf_s("%d",  &a[i][j]);
+		}                
+	}
+	printf("Введите элемент матрицы b \n");
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j < m; j++)
+		{
+			printf ("b[%d][%d]=",i,j );
+			scanf_s("%d",&b[i][j]);
+		}
+	}
+	printf("Сумма матриц= \n");
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j < m; j++)
+		{
+			c[i][j] = (a[i][j] + b[i][j]);
+		    printf("%3d",c[i][j]);	
+		}
+		printf("\n");
+	}
+
+	return 0;
+}
