6536a3f0dee08e41d5c23a42f99eea3b7187c37a
TimashaPirojok <72824293+TimashaPirojok@users.noreply.github.com>
2020-10-28 03:08:49 +0300

    Add files via upload

---
+++ лаба номер три.cpp
@@ -0,0 +1,26 @@
+﻿#include <stdio.h>
+
+int main()
+{
+	int number, a[6], k = 0;
+	for (int i = 10; i <= 999999; i++)
+	{
+		number = i;
+		for (int j = 5; j >= 0; j--)
+		{
+			a[j] = number % 10;
+			number /= 10;
+		}
+		if (a[0] + a[1] + a[2] == a[3] + a[4] + a[5])
+		{
+			k++;
+			printf("%d%d%d%d%d%d\n", a[0], a[1], a[2], a[3], a[4], a[5]);
+			
+		}
+		
+	}
+	printf(" \n");
+	printf("kolichestvo shastlivih chisel   ");
+	printf("%d\n", k);
+	return 0;
+}
