8d95b6fc10eb9f30c0ea0a6ac31c262b7e8107b6
TimashaPirojok <72824293+TimashaPirojok@users.noreply.github.com>
2020-11-24 22:36:23 +0300

    Update 67677676767667676767676.cpp

--- 67677676767667676767676.cpp
+++ 67677676767667676767676.cpp
@@ -1,48 +1,67 @@
-﻿#include <iostream>
-#include <time.h>
-#include <locale.h>
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
+#include <malloc.h>
 #include <stdlib.h>
 
-using namespace std;
-int main()
-{
-	setlocale(LC_ALL, "Russia");
-	srand(time(NULL));
-	int x,  n,  t = 0;
-	const int size = 100;
-	int arr[size][size], arr2[size];
+ int main()
+{  
+    int** a;  // указатель на указатель на строку элементов
+    int i, j, n, m, x, t=0;
+    system("cls");
+    printf("Введите количество строк: ");
+    scanf_s("%d", &n);
+    int* b = new int[n];
+    // Выделение памяти под указатели на строки
+    a = (int**)malloc(n * sizeof(int*));
+    // Ввод элементов массива
+    for (i = 0; i < n; i++)  // цикл по строкам
+    {
+        // Выделение памяти под хранение строк
+        a[i] = (int*)malloc(n*2 * sizeof(int));
+        for (j = 0; j < n*2; j++)  // цикл по столбцам
+        {
+            printf("a[%d][%d] = ", i, j);
+            scanf_s("%d", &a[i][j]);
+        }
+    }
+    // Вывод элементов массива
+    for (i = 0; i < n; i++)  // цикл по строкам
+    {
+        for (j = 0; j < n*2; j++)  // цикл по столбцам
+        {
+            printf("%5d ", a[i][j]); // 5 знакомест под элемент массива
+        }
+        printf("\n");
+    }
 
-
-	printf ("введите n ");
-	scanf_s ("%d", &n);
-	for (int i = 0; i < n; i++)
-	{
-		for (int j = 0; j < n * 2; j++)
-		{
-			arr[i][j] = rand() % 100;
-			printf ("%d\t", arr[i][j]);
-		}
-		printf("\n");
-	}
-	printf("введите х ");
-	scanf_s("%d", &x);
-	for (int i = 0; i < n; i++)
-	{
-		for (int j = 0; j < n * 2; j++)
-		{
-			if (arr[i][j] <= x)
-				t++;
-		}
-		if (t == 2 * n)
-			arr2[i] = 1;
-		else arr2[i] = 0 ;
-		t = 0;
-	}
-	printf( "\n");
-	for (int i = 0; i < n; i++)
-	{
-		printf ("%d\n", arr2[i]);
-	}
-	return(0);
+    printf("введите х ");
+    scanf_s("%d", &x);
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < 2 * n; j++)
+        {
+            if (a[i][j] <= x)
+                t++;
+        }
+        if (t == 2 * n)
+        {
+            b[i] = 1;
+        }
+        else
+        {
+            b[i] = 0;
+        }
+        t = 0;
+    }
+    printf("\n");
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d\n", b[i]);
+    }
+    // Очистка памяти
+    for (i = 0; i < n; i++)  // цикл по строкам
+        free(a[i]);   // освобождение памяти под строку
+    free(a);
+    getchar();   getchar();
+    return 0;
 }
