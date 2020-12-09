424683dacd27e7db103e80c218b91b7ec926bdcb
TimashaPirojok <72824293+TimashaPirojok@users.noreply.github.com>
2020-12-09 19:29:52 +0300

    Лаба 7

---
+++ 1111232.cpp
@@ -0,0 +1,31 @@
+﻿#include <stdio.h>
+#include <stdio.h>
+#include <ctype.h>
+#include <locale.h>
+#include <assert.h>
+#include <stdlib.h>
+int main(void)
+{
+    char str[256], s = '.';
+    char* p = str;
+
+    setlocale(LC_ALL, "");
+    printf("Введите строку\n");
+    fgets(str, 256, stdin);
+    printf("Ваша строку\n");
+    puts(str);
+    for (p = str; *p; ++p)
+    {
+        if (isupper(*(unsigned char*)p))
+            *p = tolower(*(unsigned char*)p);
+    }
+    for (int i = 0; i < 256; i++)
+    {
+        if (str[i] == '.')
+        {
+            str[i++] = (char)toupper(str[++i]);
+        }
+    }
+    puts(str);
+    return 0;
+}
